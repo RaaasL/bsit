3abcca97a87b0f916c2117e922282d1de2052958
RasL <k.hubert.szymanski@gmail.com>
2020-11-16 15:25:04 +0100

    copy

--- elements/copyright.php
+++ elements/copyright.php
@@ -1,1 +1,6 @@
-<p>All rights reserved - <a href="http://bsit.pl/">BS IT</a> - <a href="http://hubertszymanski.pl/" targer="_blank">Hubert Szymański</a> - 2020</p>
+<p id="copy"></p>
+<script>
+var d = new Date()
+var y = d.getFullYear()
+document.getElementById('copy').innerHTML = 'All rights reserved - <a href="http://bsit.pl/">BS IT</a> - <a href="http://relike.net/" targer="_blank">Hubert Szymański</a> - '+y+''
+</script>
