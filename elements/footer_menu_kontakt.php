a9ef0dfcc9bcd0ebd6236526a972c71b0bab42eb
RasL <k.hubert.szymanski@gmail.com>
2020-11-27 11:15:56 +0100

    mobile_done

--- elements/footer_menu_kontakt.php
+++ elements/footer_menu_kontakt.php
@@ -5,6 +5,6 @@
     <li><a href="https://goo.gl/maps/YAKvT8gLYvWQewLg7" target="_blank"><span>ul. Ku Słońcu 32</span></a></li>
     <li><span>71-080 Szczecin</span></li>
     <li><span>NIP: 852-25-17-717</span></li>
-    <li><a href="tel:+48503468650"><span>503-468-650</span></a></li>
+    <li><a href="tel:+48503468650"><span>tel: 503-468-650</span></a></li>
     <li><a href="mailto:bartosz.slupny@bsit.pl"><span>bartosz.slupny@bsit.pl</span></a></li>
 </ul>
--- index.php
+++ index.php
@@ -441,9 +441,18 @@
                             <li><a href="https://goo.gl/maps/YAKvT8gLYvWQewLg7" target="_blank"><span>ul. Ku Słońcu 32</span></a></li>
                             <li><span>71-080 Szczecin</span></li>
                             <li><span>NIP: 852-25-17-717</span></li>
-                            <li><a href="tel:+48503468650"><span>503-468-650</span></a></li>
+                            <li><a href="tel:+48503468650"><span>tel: 503-468-650</span></a></li>
                             <li><a href="mailto:bartosz.slupny@bsit.pl"><span>bartosz.slupny@bsit.pl</span></a></li>
                         </ul>
+                    </div>
+                    <div class="contact_details_mobile">
+                        <span class="contact_details_title">BS IT</span><br>
+                        <span>Bartosz Słupny</span><br>
+                        <a href="https://goo.gl/maps/YAKvT8gLYvWQewLg7" target="_blank"><span>ul. Ku Słońcu 32</span></a><br>
+                        <span>71-080 Szczecin</span><br>
+                        <span>NIP: 852-25-17-717</span><br>
+                        <a href="tel:+48503468650"><span>tel: 503-468-650</span></a><br>
+                        <a href="mailto:bartosz.slupny@bsit.pl"><span>bartosz.slupny@bsit.pl</span></a>
                     </div>
                 </div>
             </div>
